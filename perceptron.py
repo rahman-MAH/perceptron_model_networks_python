b7b02fcd62e29604764e09895b0e0404467fce5e
MD Abdul Haq <52556735+rahman-MAH@users.noreply.github.com>
2020-02-12 22:44:18 +0530

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,14 +1,20 @@
+# Title: Perceptron model for only a pair of inputs and its targeted output
+# Author: Md. Abdul Haq
+# Date: 12th February,2020
+
+
 # This is the perceptron model which was proposed by Rosenblatt.
 
 # It is a more general computational model than MP neuron model.
 
-# This code generates output with respect to only one case at a time.
-# Here x1 = first elements of functional bi-polar table
-#      x2 = second elements of functional bi-polar table
-#      t = targeted output of the functional bi-polar table In
-# this code if you want to assign your own weights then you can do it when the output window shows do you wan to
+# This code generates output with respect to only one case at a time i.e., "(x1,x2)" are the inputs which produces a 
+# targeted output "t"
+
+# In this code if you want to assign your own weights then you can do it when the output window shows do you want to
 # change weights.
+
 # You can cross verify you weights to check if the new weights are satisfying the whole functional bi-polar table
+
 
 
 global x1, x2, t, w1_old, w2_old, b, a, th_val, net_input, activation_function, y, w1_new, w2_new, b_new
